0b8ef80a76ad4acbbdb329bc593e6ae774d6dac7
Michal Litwin <litwinmichal4@gmail.com>
2026-05-26 18:08:14 +0200

    some work

--- python_projects/api.py
+++ python_projects/api.py
@@ -1,11 +1,13 @@
 import requests
 from interfaces import WeatherProvider
+from typing import Any
+
 
 class OpenWeatherMap(WeatherProvider):
-    def __init__(self, api_key):
+    def __init__(self, api_key: str):
         self.api_key = api_key
 
-    def get_weather(self, target):
+    def get_weather(self, target: str) ->dict[str, Any]: 
         url = f"http://api.openweathermap.org/data/2.5/forecast?q={target}&appid={self.api_key}&units=metric"
 
         response = requests.get(url)
@@ -14,7 +16,7 @@ class OpenWeatherMap(WeatherProvider):
         return response.json()
     
 class NbpCurrency:
-    def get_currency(self, target):
+    def get_currency(self, target: str) ->dict[str, Any]: 
         url = f"https://api.nbp.pl/api/exchangerates/rates/a/{target}/?format=json"
 
         response = requests.get(url)
--- python_projects/main.py
+++ python_projects/main.py
@@ -10,7 +10,7 @@ def main():
     currency_worker = NbpCurrency()
     assistant = Daily_assistant(api,currency_worker,file_save)
 
-    assistant.start_aplication("currency","EUR")
+    assistant.start_aplication("currency","AUD")
 
 if __name__ == "__main__":
     main()
