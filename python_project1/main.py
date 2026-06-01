1a62c66ae7bf70c52b58fe6e032af52465e80c23
Michal Litwin <litwinmichal4@gmail.com>
2026-06-01 14:58:23 +0200

    new name

---
+++ python_project1/main.py
@@ -0,0 +1,16 @@
+from api import OpenWeatherMap, NbpCurrency
+from storage import FileSaver
+from app import Daily_assistant
+
+
+
+def main():
+    api = OpenWeatherMap("x")
+    file_save = FileSaver("notebook.txt")
+    currency_worker = NbpCurrency()
+    assistant = Daily_assistant(api,currency_worker,file_save)
+
+    assistant.start_aplication("currency","GBP")
+
+if __name__ == "__main__":
+    main()
---
+++ python_project1/storage.py
@@ -0,0 +1,13 @@
+from interfaces import DataSaver
+import datetime
+
+class FileSaver(DataSaver):
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+
+    def save_data(self, data):
+        time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+
+        with open(self.file_name, "a", encoding="utf-8") as file:
+            file.write(f" --- {time} --- {data} \n")
